a7c559cb85ea79273237d2c741b7aa73702f3662
Aadi Shetty <aadicodes0104@gmail.com>
2026-08-02 19:48:42 +0530

    Used AI for parts of main.py will rewrite by myself after mastering the missing concepts, CORSmiddleware next

--- backend/imagescript.py
+++ backend/imagescript.py
@@ -4,35 +4,36 @@ from mainmodel import  ItemDescription
 
 load_dotenv()
 
-try:
-    def imgscript(img_b64: str, mime_type:str) -> ItemDescription:
-        client = genai.client()
-        interaction=client.interactions.create(
-            model="gemini-3.6-flash",
-            input=[
-                {
-                    "type":"text",
-                    "text": "extract the details of the given object in the image"
-                },
-                {
-                    "type":"image",
-                    "data":img_b64,
-                    "mime_type":"image/jpeg"
-                },
-            ],
-            response_format=
-                {
-                    "type": "text",
-                    "mime_type":"application/json",
-                    "schema": ItemDescription.model_json_schema()
-                },
 
-            
-        )     
-    return ItemDescription.model_validate_json(interaction.output.text)
+def imgscript(img_b64: str, mime_type:str) -> ItemDescription:
+        try:
+            client = genai.client()
+            interaction=client.interactions.create(
+                model="gemini-3.6-flash",
+                input=[
+                    {
+                        "type":"text",
+                        "text": "extract the details of the given object in the image"
+                    },
+                    {
+                        "type":"image",
+                        "data":img_b64,
+                        "mime_type":"image/jpeg"
+                    },
+                ],
+                response_format=
+                    {
+                        "type": "text",
+                        "mime_type":"application/json",
+                        "schema": ItemDescription.model_json_schema()
+                    },
+
+                
+            )     
+            return ItemDescription.model_validate_json(interaction.output.text)
 
 
-except Exception as e:
-    print(type(e))
-    print(f"API error: {e}")
-    break
+        except Exception as e:
+            print(type(e))
+            print(f"API error: {e}")
+            raise
